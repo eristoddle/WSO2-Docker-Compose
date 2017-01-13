4217dc0cae38560c5dea6cae8b297ee5d0722177
Stephan MIller <stephanmil@gmail.com>
2017-01-13 16:31:31 -0600

    is using only postgres dbs

--- conf/wso2scripts/postgres-init/run-sql.sh
+++ conf/wso2scripts/postgres-init/run-sql.sh
@@ -8,12 +8,21 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE registrydb TO wso2user;
     CREATE DATABASE identitydb;
     GRANT ALL PRIVILEGES ON DATABASE identitydb TO wso2user;
+    CREATE DATABASE identitymetricsdb;
+    GRANT ALL PRIVILEGES ON DATABASE identitymetricsdb TO wso2user;
+    CREATE DATABASE identitycarbondb;
+    GRANT ALL PRIVILEGES ON DATABASE identitycarbondb TO wso2user;
+    CREATE DATABASE identitybpeldb;
+    GRANT ALL PRIVILEGES ON DATABASE identitybpeldb TO wso2user;
 EOSQL
 
 echo "Initializing database WSO2 databases mbstoredb, registrydb and identitydb"
 psql --username="$POSTGRES_USER" -d mbstoredb -f /tmp/postgres-sql/apim-init.sql
 psql --username="$POSTGRES_USER" -d identitydb -f /tmp/postgres-sql/carbon-init.sql
 psql --username="$POSTGRES_USER" -d registrydb -f /tmp/postgres-sql/carbon-init.sql
+psql --username="$POSTGRES_USER" -d identitymetricsdb -f /tmp/postgres-sql/metrics-init.sql
+psql --username="$POSTGRES_USER" -d identitycarbondb -f /tmp/postgres-sql/carbon-init.sql
+psql --username="$POSTGRES_USER" -d identitybpeldb -f /tmp/postgres-sql/bpel-init.sql
 
 # Add conf
 cp /tmp/postgres-sql/postgresql.conf /var/lib/postgresql/data/postgresql.conf
