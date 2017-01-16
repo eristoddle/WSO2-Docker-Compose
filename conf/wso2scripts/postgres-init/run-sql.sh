9dcb3580fbcbf74e62938b74323d1648791a0450
Stephan MIller <stephanmil@gmail.com>
2017-01-16 16:17:19 -0600

    still fixing is and greg carbon postgres dbs

--- conf/wso2scripts/postgres-init/run-sql.sh
+++ conf/wso2scripts/postgres-init/run-sql.sh
@@ -34,25 +34,32 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE amcarbondb TO wso2user;
     CREATE DATABASE ammetricsdb;
     GRANT ALL PRIVILEGES ON DATABASE ammetricsdb TO wso2user;
+    CREATE DATABASE ascarbondb;
+    GRANT ALL PRIVILEGES ON DATABASE ascarbondb TO wso2user;
 EOSQL
 
 echo "Initializing database WSO2 databases"
 psql --username="$POSTGRES_USER" -d mbstoredb -f /tmp/postgres-sql/mb-init.sql
 psql --username="$POSTGRES_USER" -d identitydb -f /tmp/postgres-sql/carbon-init.sql
 psql --username="$POSTGRES_USER" -d registrydb -f /tmp/postgres-sql/carbon-init.sql
+
 psql --username="$POSTGRES_USER" -d identitymetricsdb -f /tmp/postgres-sql/metrics-init.sql
-psql --username="$POSTGRES_USER" -d identitycarbondb -f /tmp/postgres-sql/carbon-init.sql
+# psql --username="$POSTGRES_USER" -d identitycarbondb -f /tmp/postgres-sql/carbon-is-init.sql
 psql --username="$POSTGRES_USER" -d identitybpeldb -f /tmp/postgres-sql/bpel-init.sql
-psql --username="$POSTGRES_USER" -d esbcarbondb -f /tmp/postgres-sql/carbon-init.sql
-psql --username="$POSTGRES_USER" -d brscarbondb -f /tmp/postgres-sql/carbon-init.sql
-psql --username="$POSTGRES_USER" -d dsscarbondb -f /tmp/postgres-sql/carbon-init.sql
-psql --username="$POSTGRES_USER" -d gregcarbondb -f /tmp/postgres-sql/carbon-init.sql
+
+# psql --username="$POSTGRES_USER" -d gregcarbondb -f /tmp/postgres-sql/carbon-greg-init.sql
 psql --username="$POSTGRES_USER" -d gregmetricsdb -f /tmp/postgres-sql/metrics-init.sql
 psql --username="$POSTGRES_USER" -d gregbpeldb -f /tmp/postgres-sql/bpel-init.sql
 psql --username="$POSTGRES_USER" -d gregsocialdb -f /tmp/postgres-sql/social-init.sql
+
 psql --username="$POSTGRES_USER" -d apimgtdb -f /tmp/postgres-sql/apimgt-init.sql
 psql --username="$POSTGRES_USER" -d amcarbondb -f /tmp/postgres-sql/carbon-init.sql
 psql --username="$POSTGRES_USER" -d ammetricsdb -f /tmp/postgres-sql/metrics-init.sql
+
+psql --username="$POSTGRES_USER" -d esbcarbondb -f /tmp/postgres-sql/carbon-init.sql
+psql --username="$POSTGRES_USER" -d brscarbondb -f /tmp/postgres-sql/carbon-init.sql
+psql --username="$POSTGRES_USER" -d dsscarbondb -f /tmp/postgres-sql/carbon-init.sql
+psql --username="$POSTGRES_USER" -d ascarbondb -f /tmp/postgres-sql/carbon-init.sql
 
 # Add conf
 cp /tmp/postgres-sql/postgresql.conf /var/lib/postgresql/data/postgresql.conf
